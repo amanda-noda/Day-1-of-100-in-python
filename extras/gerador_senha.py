60358fc7651b200d97449dafe56b5d87518071da
AMANDA FERNANDES CUSTODIO <58089751+amanda-noda@users.noreply.github.com>
2023-05-30 21:33:45 -0300

    Update gerador_senha.py

--- extras/gerador_senha.py
+++ extras/gerador_senha.py
@@ -6,6 +6,7 @@ def gerar_senha(tamanho):
     senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
     return senha
 
+## Gera uma senha de acordo com o tamanho que vc digitar, caso queira uma senha de 8 digitos, o gerador gera uma senha de 8 digitos e assim por diante.
 tamanho_senha = int(input("Digite o tamanho desejado para a senha: "))
 senha_gerada = gerar_senha(tamanho_senha)
 print("Senha gerada:", senha_gerada)
