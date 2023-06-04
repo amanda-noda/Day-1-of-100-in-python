8dc09759c6781825027400876d1727295727dd8c
Amanda Fernandes Cutódio <58089751+amanda-noda@users.noreply.github.com>
2023-06-03 22:55:59 -0300

    Adiciona dia 2 e extra

---
+++ day_2/imc.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+imc =  int (weight)/float(height)**2
+imc_int = int (imc)
+print (imc_int)
---
+++ day_2/soma_num2dig.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Type a two digit number: ")
+
+primeiro_num = two_digit_number[0]
+segundo_num = two_digit_number[1]
+
+resultado = int (primeiro_num) + int (segundo_num)
+print (resultado)
